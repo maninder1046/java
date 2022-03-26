72c5b6bdd2c1b53fa3f4ff40d475fb3356e41504
maninder1046 <90817762+maninder1046@users.noreply.github.com>
2022-03-26 14:28:39 +0530

    Create p1.java

---
+++ java1/p1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Student{
+ int rollno;
+ String name;
+ String branch;
+ static String university="Graphic Era Hill University ";
+void set(String name ,int rollno ,String branch)
+{
+  this.name=name;
+  this.rollno=rollno;
+  this.branch=branch;
+}
+void get()
+{
+  System.out.println("Name " + name + "\nBranch " + branch + "\nrollno " + rollno + "\nuniversity" + university);
+
+}
+public static void main(String args[])
+{
+  Scanner sc= new Scanner(System.in);
+  Student s1=new Student();
+  String name ,branch;
+  int rollno;
+  System.out.println("enter name  :");
+  name =sc.nextLine();
+  System.out.println("enter rollno");
+  rollno =sc.nextInt();
+  System.out.println("enter branch");
+  branch =sc.nextLine();
+  System.out.println("enter university");
+  university =sc.nextLine();
+  s1.set(name,rollno,branch);
+  s1.get();
+  
+}
+}
